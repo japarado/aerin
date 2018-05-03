91e467aefc5647b9c694bbf1fd43d5608300fc46
Justin Parado <japarado@student.apc.edu.ph>
2018-05-03 15:21:24 +0800

    Shell Script that: 1) Deletes current sqlite file 2) Deletes migration files 3) Makes migrations and migrates 4) Creates a super user   name = admin   pass = password123

---
+++ delete_migrate_superuser.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd ~/PycharmProjects/drf/
+rm db.sqlite3
+find ~/PycharmProjects/drf/snippets/migrations/ -name "0*" -exec rm {} \;
+python3.6 manage.py makemigrations
+python3.6 manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'password123')" | python3.6 manage.py shell
