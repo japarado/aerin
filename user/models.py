7add52b20e6bcfbb27ba102e0278521235d40e30
Justin Parado <japarado@student.apc.edu.ph>
2018-05-22 10:40:31 +0800

    User Models
    Created a base class for all other in-site users + registered the app in settings.py

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+"""Generic User"""
+"""Staff == can log in into admin site.
+SuperUser == admin site + all CRUD privileges there"""
+class Player(AbstractUser):
+    is_superuser = False
+    is_staff = False
